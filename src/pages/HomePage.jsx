0a98b3eecb21c0eb299acef20d63d59fbd401003
Luciano Yomayel <l.yomayel@gmail.com>
2023-08-01 11:21:24 +1000

    ok

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@
 export function HomePage() {
     return (
         <div className="home-page">
-            <h1>Octavo Jugador 123 Ahora sii</h1>
+            <h1>Octavo Jugador 1234 Ahora sii</h1>
             <p>Esta es la página de inicio</p>
         </div>
 
